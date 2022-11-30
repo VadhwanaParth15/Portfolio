b169d6c40b50f9a0173c0eb02ea93c66b4d2d617
vadhwana parth <119338385+VadhwanaParth15@users.noreply.github.com>
2022-11-30 20:04:21 +0530

    Add files via upload
    my portfolio

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+         $('.navbar .menu').toggleClass("active");
+         $('.menu-btn i').toggleClass("active");
+    });
+})
+
+// typing text animation script
+var typed = new Typed(".typing-1", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+ 
