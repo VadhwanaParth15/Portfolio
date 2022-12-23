b118b6d27dc7dee64112237ec6b53983f32f860f
vadhwana parth <119338385+VadhwanaParth15@users.noreply.github.com>
2022-12-23 20:33:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,17 +12,5 @@ $(document).ready(function(){
     });
 })
 
-// typing text animation script
-var typed = new Typed(".typing", {
-    strings: ["Web-Devloper", "App-Developer", "Data-analyst"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-var typed = new Typed(".typing-2", {
-    strings: ["Web-Devloper", "App-Developer", "Data-analyst"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
+
  
