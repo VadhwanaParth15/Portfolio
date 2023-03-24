0c391362b270dbb1f08b6e6a6efbbd9db993dc77
VadhwanaParth15 <parthvadhwana050@gmail.com>
2023-03-24 12:19:51 +0530

    resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import CV from '../../assets/resume.pdf'
+import CV from '../../assets/CV.pdf'
 const CTA = () => {
   return (
     <div className='CTA'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download className='btn'>Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk </a>
     </div>
   )
