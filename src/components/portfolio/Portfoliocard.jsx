eb73c7588cc28f72ee7cadbfd7b52b0b57571b17
Parth <parthvadhwana050@gmail.com>
2023-11-21 19:35:33 +0530

    comit

--- src/components/portfolio/Portfoliocard.jsx
+++ src/components/portfolio/Portfoliocard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Col } from "react-bootstrap";
+import { motion } from "framer-motion"
 // import {AiFillGithub} from "react-icons/ai";
 // import {BsArrowUpRightCircleFill} from "react-icons/bs";
 const portfoliocard  = ({ title, imgUrl,  link }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <article className='portfolio__item' >
+      <motion.article className='portfolio__item' whileHover={{ scale: 1.2 }}
+  onHoverStart={e => {}}
+  onHoverEnd={e => {}}>
         <div className="portfolio__item-image">
           <img src={imgUrl} alt="" />
         </div>
@@ -14,7 +17,7 @@ const portfoliocard  = ({ title, imgUrl,  link }) => {
         <a href="https://github.com/VadhwanaParth15" className="btn" target="_blank">GitHub</a>
         <a href={link} className="btn btn-primary" target="_blank">LIVE</a>
         </div>
-      </article>
+      </motion.article>
     </Col>
   )
 }
