734525f1517585f0a725c5b42b209820edb53e70
Parth <parthvadhwana050@gmail.com>
2024-05-23 12:05:04 +0530

    css

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -19,7 +19,7 @@ const experiences = [
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
     contentStyle={{
-      background: 'transparent',
+      background: '#323946',
       color: '#292929',
       boxShadow: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
       borderRadius: '10px',
