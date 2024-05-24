a99a5c0b1c2e25f221681a54bcc4388f01df398a
Parth <parthvadhwana050@gmail.com>
2024-05-24 13:08:11 +0530

    twitter

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './footer.css'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import { BsTwitter , BsInstagram} from 'react-icons/bs'
+import { BsInstagram} from 'react-icons/bs'
+import {FaXTwitter} from 'react-icons/fa6'
 const footer = () => {
   return (
     <footer className="footer">
@@ -10,7 +11,7 @@ const footer = () => {
        
         <a href="https://www.linkedin.com/in/parth-vadhwana-74b623220" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/VadhwanaParth15" target="_blank"><BsGithub/></a>
-        <a href="https://www.linkedin.com/in/parth-vadhwana-74b623220" target="_blank"><BsTwitter/></a>
+        <a href="https://x.com/pxrth_15?t=0N2Tq4XcJBixtMHkRc-pAw&s=09" target="_blank"><FaXTwitter /></a>
         <a href="https://instagram.com/pxrth.02?igshid=ZDdkNTZiNTM=" target="_blank"><BsInstagram/></a>
     </div>
         <p className="company-name">
--- src/components/header/headerSocial.jsx
+++ src/components/header/headerSocial.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import {BsTwitter} from 'react-icons/bs'
+import {FaXTwitter} from 'react-icons/fa6'
 
 const headerSocial = () => {
   return (
     <div className="header__social" >
         <a href="https://www.linkedin.com/in/parth-vadhwana-74b623220" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/VadhwanaParth15" target="_blank"><BsGithub/></a>
-        <a href="https://www.linkedin.com/in/parth-vadhwana-74b623220" target="_blank"><BsTwitter/></a>
+        <a href="https://x.com/pxrth_15?t=0N2Tq4XcJBixtMHkRc-pAw&s=09" target="_blank"><FaXTwitter /></a>
     </div>
   )
 }
